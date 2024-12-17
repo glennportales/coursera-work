2bb240870bf9a27d009ddbc6196df25bf8e870ba
Glennisio <glenn.rivera1@yahoo.com>
2024-12-17 10:59:31 -0400

    array destructuring practice

--- scrimba/JavaScriptDeepDive/lunchMenu3.js
+++ scrimba/JavaScriptDeepDive/lunchMenu3.js
@@ -3,14 +3,17 @@ const finalMenuItems = [
     "Southern Fried Chicken",
     "Glazed Salmon"
   ];
+
   
-  const [winner, ...losers] = finalMenuItems;
-  
-  console.log({ winner, losers });
+  // console.log({ winner, losers });
   // let [first, second] = finalMenuItems;
   // [second, first] = [first, second];
   // let first = finalMenuItems[0];
   // let second = finalMenuItems[1];
   // let third = finalMenuItems[2];
   
-  // console.log(first, second, third);
+  //we can swap values using destructuring
+
+const [winner, ...losers] = finalMenuItems;
+
+console.log({ losers} );
